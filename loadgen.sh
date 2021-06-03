823632f635fb357f9a42055260952476f70aad96
Peter Stone <stonepe@vmware.com>
2021-06-03 13:10:43 -0600

    add loadgen.sh

---
+++ loadgen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+	quantity=$((1 + RANDOM % 20))
+	curl_cmd="curl -X POST http://localhost:50050/shop/order -H 'Content-Type: application/json' -d '{\"styleName\": \"beachops\", \"quantity\": '$quantity'}'"
+	echo $curl_cmd
+	eval $curl_cmd
+	sleep ${1:-1}
+	echo
+done
